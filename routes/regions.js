c9c6f55d039e996747728a35aa958778602d7b10
ahmedBudUser <prochickk@gmail.com>
2023-08-31 15:44:03 +0300

    cleaning

--- routes/regions.js
+++ routes/regions.js
@@ -5,11 +5,10 @@ const Regions = require('../module/Regions');
 const auth = require("../middleware/auth");
 
 router.get("/", async (req, res) => {
-  console.log('hi')
+
   try {
 
     if (req.query.driverGroup != "New"){
-      console.log('False')
       let groupRegions = await Regions.find({group: req.query.driverGroup})
 
       if (!groupRegions[0]) return res.status(404).send('Group is not found')
@@ -18,7 +17,6 @@ router.get("/", async (req, res) => {
     }
 
     if (req.query.driverGroup == "New"){
-      console.log('True')
       let groupRegions = await Regions.find({groupList: {$exists: true}}); 
       res.status(200).send(groupRegions);
     }
